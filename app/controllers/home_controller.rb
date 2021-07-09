bee6eecf763d5783b629baf69421d7b0f95031d7
Moonwalker <abhisk.mhra@gmail.com>
2021-07-09 13:12:36 +0530

    Doc : homecontroller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,10 @@
 class HomeController < ApplicationController
+
+  # this controller is for /home route
+
   def index
+    # this function corelates to /home/index.html.erb page
+    # this code works for the correlated web page
+
   end
 end
