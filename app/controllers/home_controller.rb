be7dfe0c84007b5c54ef17faebba4ef567f75f33
Moonwalker <abhisk.mhra@gmail.com>
2021-07-09 13:17:30 +0530

    new Route : /home/about.html.erb added

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,6 +5,10 @@ class HomeController < ApplicationController
   def index
     # this function corelates to /home/index.html.erb page
     # this code works for the correlated web page
-
   end
+
+  def about 
+    # this function corelates to /home/about.html.erb page
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   # get 'home/index' 
   root "home#index" # home page
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # about.html.erb
+  get "home/about"
+
 end
