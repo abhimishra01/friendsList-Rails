a6a0d2e5e3d03b03baf3d5e54d6b98d88b632bf1
Moonwalker <abhisk.mhra@gmail.com>
2021-07-09 16:37:53 +0530

    new Friends CRUD Routes added

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   
   resources :friends # this is way to automate thing for CRUD rather creating 10 diff routes like add create update routes etc
   # get 'home/index' 
-  root "home#index" # home page
+  root "friends#index" # home page
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   # about.html.erb
