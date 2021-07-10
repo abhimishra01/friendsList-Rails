16b86016d01c58d4e33893d3a61da866c6f3929c
Moonwalker <abhisk.mhra@gmail.com>
2021-07-10 11:41:43 +0530

    WIP : Friends and Users DB Associations

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,2 +1,6 @@
 class Friend < ApplicationRecord
+
+  # Association with "Users DB"
+    belongs_to :user
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # Association with "Friends DB"
+  has_many :friends
 end
